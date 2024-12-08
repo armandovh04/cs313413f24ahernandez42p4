bf81ca06d057ea39014f36eba2d11b45a428745e
Armando Hernandez <120071751+armandovh04@users.noreply.github.com>
2024-12-07 19:41:22 -0600

    Update DefaultStopwatchStateMachine.java

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/DefaultStopwatchStateMachine.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/DefaultStopwatchStateMachine.java
@@ -41,30 +41,41 @@ public class DefaultStopwatchStateMachine implements StopwatchStateMachine {
     // these must be synchronized because events can come from the
     // UI thread or the timer thread
     @Override public synchronized void onStartStop() { state.onStartStop(); }
-    @Override public synchronized void onLapReset()  { state.onLapReset(); }
-    @Override public synchronized void onTick()      { state.onTick(); }
-
+    @Override public synchronized void onTick() { state.onTick(); }
+    
     @Override public void updateUIRuntime() { listener.onTimeUpdate(timeModel.getRuntime()); }
-    @Override public void updateUILaptime() { listener.onTimeUpdate(timeModel.getLaptime()); }
+    @Override public void startAlarm() { listener.playAlarm(); }
 
     // known states
     private final StopwatchState STOPPED     = new StoppedState(this);
     private final StopwatchState RUNNING     = new RunningState(this);
-    private final StopwatchState LAP_RUNNING = new LapRunningState(this);
-    private final StopwatchState LAP_STOPPED = new LapStoppedState(this);
+    private final StopwatchState ALARMED       = new AlarmState(this);
+    private final StopwatchState LAP_STOPPED = new LapStoppedState(this); //need to change to new state where it goes down in time
 
     // transitions
     @Override public void toRunningState()    { setState(RUNNING); }
     @Override public void toStoppedState()    { setState(STOPPED); }
-    @Override public void toLapRunningState() { setState(LAP_RUNNING); }
-    @Override public void toLapStoppedState() { setState(LAP_STOPPED); }
+    @Override public void toAlarmState() { setState(ALARMED); }
+    @Override public void toLapStoppedState() { setState(LAP_STOPPED); } //same here
 
     // actions
     @Override public void actionInit()       { toStoppedState(); actionReset(); }
     @Override public void actionReset()      { timeModel.resetRuntime(); actionUpdateView(); }
+    
     @Override public void actionStart()      { clockModel.start(); }
     @Override public void actionStop()       { clockModel.stop(); }
+
+    @Override
+    public void clockReset() {
+        clockModel.reset();
+    }
+    
     @Override public void actionLap()        { timeModel.setLaptime(); }
     @Override public void actionInc()        { timeModel.incRuntime(); actionUpdateView(); }
     @Override public void actionUpdateView() { state.updateView(); }
+
+    @Override
+    public int getTime() {
+        return timeModel.getRuntime();
+    }
 }
